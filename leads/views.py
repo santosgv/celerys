4b28621c8193ef7b70bfbbaccb31f15219f2b842
Vitor.Gomes <santosgomesv@gmail.com>
2022-03-15 11:56:38 -0300

    gitignore

---
+++ leads/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+def home(request):
+    status = request.GET.get('status')
+    return render(request,'principal.html')
+
+def valida(request):
+    nome =request.POST.get('nome')
+    email =request.POST.get('email')
+
+
+    if len(nome) < 3 or len(email) < 10:
+        return HttpResponse('Email ou senha invalidos')
+
+    try:
+        return render(request, 'envio.html',{'nome':nome,
+                                            'email':email})
+    except:
+        return HttpResponse('erro interno do sistema')
+
+
